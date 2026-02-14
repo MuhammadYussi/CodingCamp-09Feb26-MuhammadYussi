c62e5aad4e12416a06eaf1276d5c718e0090ca71
MuhammadYussi <muhammadyussi@hotmail.com>
2026-02-14 09:28:16 +0700

    update html & javascript for functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 const form = document.getElementById('form');
-const displayList = document.querySelector('.flex.justify-around div ul');
+const displayList = document.getElementById('message');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const nama = form.querySelector('input[type="text"]').value;
     const tglLahir = form.querySelector('input[type="date"]').value;
-    const gender = form.querySelector('input[type="radio"]:checked')?.value || "Tidak disebutkan";
+    const gender = form.querySelector('input[type="radio"]').value;
     const pesan = form.querySelector('textarea').value;
     
     const now = new Date();
     const dateTimeString = now.toLocaleString();
 
-    const listItem = document.createElement('li');
-    listItem.className = "mb-4 p-3 border-b border-sky-200 last:border-0";
+    const listItem = document.createElement('ul');
+    listItem.className = "mb-4 p-3 border-b border-black/20 last:border-0";
     
     listItem.innerHTML = 
         `<small class="text-gray-500 italic">${dateTimeString}</small>
         <p><strong>Nama:</strong> ${nama}</p>
         <p><strong>Tgl Lahir:</strong> ${tglLahir}</p>
-        <p><strong>Jenis Kelamin:</strong> ${gender}</p>
+        <p><strong>Gender:</strong> ${gender}</p>
         <p><strong>Pesan:</strong> ${pesan}</p>`;
 
     displayList.appendChild(listItem);
